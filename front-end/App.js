f02066b8995eea7f2fe26197aee36afbb2b179aa
Abdul Jawwad <abduljawwad.md@gmail.com>
2021-09-23 15:11:53 +0100

    modified navigator

--- front-end/App.js
+++ front-end/App.js
@@ -1,22 +1,33 @@
-import React, { useState, useContext } from "react";
-import { Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Ionicons from "react-native-vector-icons/Ionicons";
-import WelcomeScreen from "./Screens/WelcomeScreen";
-import HomeScreen from "./Screens/HomeScreen";
-import ReadingScreen from "./Screens/ReadingScreen";
-import ReadScreen from "./Screens/ReadScreen";
-import LoginScreen from "./Screens/LoginScreen";
-import SettingsScreen from "./Screens/SettingsScreen";
-import * as firebase from "firebase";
-import { firebaseConfig } from "./Config/config";
-import "firebase/auth";
-import BooksContextProvider from "./Context/BooksContextProvider";
+import React, { useState, useContext } from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import WelcomeScreen from './Screens/WelcomeScreen';
+import HomeScreen from './Screens/HomeScreen';
+import ReadingScreen from './Screens/ReadingScreen';
+import ReadScreen from './Screens/ReadScreen';
+import LoginScreen from './Screens/LoginScreen';
+import SignupScreen from './Screens/SignupScreen';
+import SettingsScreen from './Screens/SettingsScreen';
+import * as firebase from 'firebase';
+import { firebaseConfig } from './Config/config';
+import 'firebase/auth';
+import BooksContextProvider from './Context/BooksContextProvider';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+
+function MainPages () {
+  return (
+    <Tab.Navigator initialRouteName="Home" screenOptions={{headerShown:false}}>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Reading" component={ReadingScreen} />
+      <Tab.Screen name="Read" component={ReadScreen} />
+    </Tab.Navigator>
+  )
+}
 
 export default function App({ navigation }) {
   const [isSignedIn, setIsSignedIn] = useState(true);
@@ -31,8 +42,7 @@ export default function App({ navigation }) {
 
   const initializeFirebaseFn = initializeFirebase();
 
-  const [initializeFirebaseVar, setInitializeFirebaseVar] =
-    useState(initializeFirebaseFn);
+  const [initializeFirebaseVar, setInitializeFirebaseVar] = useState(initializeFirebaseFn);
 
   const signIn = (a) => {
     setIsSignedIn(a);
@@ -41,11 +51,11 @@ export default function App({ navigation }) {
   return (
     <NavigationContainer>
       <BooksContextProvider>
-        <Tab.Navigator>
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="Reading" component={ReadingScreen} />
-          <Tab.Screen name="Read" component={ReadScreen} />
-        </Tab.Navigator>
+        <Stack.Navigator initialRouteName='Login' screenOptions={{headerShown:false}}>
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Signup" component={SignupScreen} />
+          <Stack.Screen name="Main" component={MainPages} />
+        </Stack.Navigator>
       </BooksContextProvider>
     </NavigationContainer>
   );
