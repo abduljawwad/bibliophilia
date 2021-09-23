cb91cd29af874f87523d313b9d9cc92a8a5b831c
Abdul Jawwad <abduljawwad.md@gmail.com>
2021-09-23 15:11:33 +0100

    added signup and login API

---
+++ back-end/server.js
@@ -0,0 +1,26 @@
+require('./config/db')
+const express = require("express");
+const app = express();
+const validate = require("express-jsonschema").validate;
+const helmet = require("helmet");
+const morgan = require("morgan");
+var bodyParser = require("body-parser");
+const UserRouter = require('./routes/User')
+
+// Add express Builtin middleware
+app.use(express.json());
+
+// Add third party middleware
+app.use(helmet());
+
+if (app.get("env") === "development") {
+  app.use(morgan("tiny"));
+  console.log("Morgan enabled...");
+}
+
+app.use('/user', UserRouter)
+
+
+
+const port = process.env.PORT || 3049;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
