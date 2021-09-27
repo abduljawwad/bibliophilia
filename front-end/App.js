b174538a08b55dfaa44deb75622a010ad1a2de5b
Abdul Jawwad <abduljawwad.md@gmail.com>
2021-09-27 19:49:59 +0100

    added navigationStack

--- front-end/App.js
+++ front-end/App.js
@@ -1,43 +1,12 @@
-import React, { useState, useContext } from 'react';
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import WelcomeScreen from './Screens/WelcomeScreen';
-import HomeScreen from './Screens/HomeScreen';
-import ReadingScreen from './Screens/ReadingScreen';
-import ReadScreen from './Screens/ReadScreen';
-import LoginScreen from './Screens/LoginScreen';
-import SignupScreen from './Screens/SignupScreen';
-import SettingsScreen from './Screens/SettingsScreen';
-import BooksContextProvider from './Context/BooksContextProvider';
+import React from 'react';
+import UserCredentialsContextProvider from './Context/UserCredentialsContextProvider';
+import NavigationStack from './Navigators/NavigationStack';
 
-const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
-
-function MainPages () {
-  return (
-    <Tab.Navigator initialRouteName="Home" screenOptions={{headerShown:false}}>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Reading" component={ReadingScreen} />
-      <Tab.Screen name="Read" component={ReadScreen} />
-    </Tab.Navigator>
-  )
-}
-
-export default function App({ navigation }) {
+export default function App() {
  
   return (
-    <NavigationContainer>
-      <BooksContextProvider>
-        <Stack.Navigator initialRouteName='Welcome' screenOptions={{headerShown:false}}>
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Signup" component={SignupScreen} />
-          <Stack.Screen name="Welcome" component={WelcomeScreen} />
-          <Stack.Screen name="Main" component={MainPages} />
-        </Stack.Navigator>
-      </BooksContextProvider>
-    </NavigationContainer>
+    <UserCredentialsContextProvider>
+      <NavigationStack />
+    </UserCredentialsContextProvider>
   );
 }
---
+++ front-end/Navigators/NavigationStack.js
@@ -0,0 +1,48 @@
+import React, { useContext } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import WelcomeScreen from '../Screens/WelcomeScreen';
+import HomeScreen from '../Screens/HomeScreen';
+import ReadingScreen from '../Screens/ReadingScreen';
+import ReadScreen from '../Screens/ReadScreen';
+import LoginScreen from '../Screens/LoginScreen';
+import SignupScreen from '../Screens/SignupScreen';
+import BooksContextProvider from '../Context/BooksContextProvider';
+import {UserCredentialsContext} from '../Context/UserCredentialsContextProvider';
+
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function MainPages () {
+  return (
+      <BooksContextProvider>
+        <Tab.Navigator initialRouteName="Home" screenOptions={{headerShown:false}}>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Reading" component={ReadingScreen} />
+          <Tab.Screen name="Read" component={ReadScreen} />
+        </Tab.Navigator>
+      </BooksContextProvider>
+  )
+}
+
+export default function NavigationStack({ navigation }) {
+	const { storedCredentials } = useContext(UserCredentialsContext)
+  console.log("🚀 ~ file: NavigationStack.js ~ line 32 ~ NavigationStack ~ storedCredentials", storedCredentials)
+ 
+  return (
+			<NavigationContainer>
+					<Stack.Navigator initialRouteName='Login' screenOptions={{headerShown:false}}>
+						{
+						storedCredentials? <Stack.Screen name="Welcome" component={WelcomeScreen} /> : 
+						<>					
+							<Stack.Screen name="Login" component={LoginScreen} />
+							<Stack.Screen name="Signup" component={SignupScreen} />
+							<Stack.Screen name="Main" component={MainPages} />
+						</>
+						}
+					</Stack.Navigator>
+			</NavigationContainer>
+  );
+}
